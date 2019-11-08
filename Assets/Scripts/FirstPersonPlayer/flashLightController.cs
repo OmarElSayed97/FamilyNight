80ad78b67b631b9bc46c57e29e174f666aa9124e
MayarKamali <mayar.lotfy@student.guc.edu.eg>
2019-11-08 12:03:19 +0200

    added first player movement

---
+++ Assets/Scripts/FirstPersonPlayer/flashLightController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashLightController : MonoBehaviour
+{
+
+    private bool flashLightEnabeled;
+
+    public GameObject flashLight;
+   // public GameObject lightObject;
+   // public float maxEnergy;
+  //  public float currentEnergy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashLightEnabeled = !flashLightEnabeled;
+        }
+
+        if (flashLightEnabeled)
+            flashLight.SetActive(true);
+        else
+            flashLight.SetActive(false);
+    }
+}
